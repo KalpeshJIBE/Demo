d1459714ff2fac06714ee2b532e46f69c95e36f1
kaustubh-seachange <kaustubh@seachange-maritime.com>
2015-04-28 09:53:20 +0530

    Create new class for add feature

---
+++ DemoKalpesh/Base.lproj/MymodifiedClass.h
@@ -0,0 +1,13 @@
+//
+//  MymodifiedClass.h
+//  DemoKalpesh
+//
+//  Created by mumbai on 28/04/15.
+//  Copyright (c) 2015 JIBE. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MymodifiedClass : NSObject
+
+@end
---
+++ DemoKalpesh/Base.lproj/MymodifiedClass.m
@@ -0,0 +1,13 @@
+//
+//  MymodifiedClass.m
+//  DemoKalpesh
+//
+//  Created by mumbai on 28/04/15.
+//  Copyright (c) 2015 JIBE. All rights reserved.
+//
+
+#import "MymodifiedClass.h"
+
+@implementation MymodifiedClass
+
+@end
