c3b5246a03da991bae57f0c41130db50d3de1f0e
kaustubh-seachange <kaustubh@seachange-maritime.com>
2015-04-28 09:21:33 +0530

    Initial Commit

---
+++ DemoKalpesh/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DemoKalpesh
+//
+//  Created by mumbai on 28/04/15.
+//  Copyright (c) 2015 JIBE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ DemoKalpesh/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  DemoKalpesh
+//
+//  Created by mumbai on 28/04/15.
+//  Copyright (c) 2015 JIBE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
